62f160040c30896a16fe8d83c027969a65debc3c
Rado Jedro <radoslaw.jedro@gmail.com>
2019-10-06 14:42:34 +0100

    Add class BaseWidget

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,67 +1,84 @@
 import {select,settings} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-
-class AmountWidget{
+/* class AmountWidget is an extension of class BaseWidget*/
+class AmountWidget extends BaseWidget{
   constructor(element){
+    /*must have super() and refers to class BaseWidget */
+    super(element,settings.amountWidget.defaultValue);
+
     const thisWidget = this;
     thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+
+    //delete dependency of extended class
+    // thisWidget.value = settings.amountWidget.defaultValue;
+    // thisWidget.setValue(thisWidget.dom.input.value);
+
     thisWidget.initActions();
-    //console.log('AmountWidget:', thisWidget);
-    //console.log('constructor arguments:', element);
-  }
-  getElements(element){
-    const thisWidget=this;
-
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    console.log('AmountWidget:',thisWidget);
 
   }
-
-  setValue(value){
+  getElements(){
     const thisWidget=this;
-    const newValue = parseInt(value);
 
-    /* TODO: Add validiation */
-    if(
-      newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax){
-      thisWidget.value = newValue;
-      thisWidget.announce();
-    }
-    thisWidget.input.value = thisWidget.value;
-    //console.log(thisWidget.input.value);
+    // thisWidget.element = element; moved to BaseWidget
+    // thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input); changed to thisWidget.dom.wrpapper
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
+
+  }
+
+  /* methods: set value and parseValue(value) have been moved to BaseWidget as does not have any dependancy in AmountWidget*/
+
+  // setValue(value){
+  //   const thisWidget=this;
+  //   const newValue = thisWidget.parseValue(value);
+
+  //   /*Add validiation */
+  //   if(
+  //     newValue != thisWidget.correctValue && thisWidget.isValid(newValue)){
+  //     thisWidget.correctValue = newValue;
+  //     thisWidget.announce();
+  //   }
+
+  //   thisWidget.renderValue();
+
+  // }
+
+  /* this method overrides parseValue(value) from class BaseWidget*/
+  isValid(value){
+    return !isNaN(value)
+      && value >= settings.amountWidget.defaultMin
+      && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue(){
+    const thisWidget=this;
+
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions(){
     const thisWidget = this;
 
-    thisWidget.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.input.value);
+    thisWidget.dom.input.addEventListener('change', function(){
+      //thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
 
-    thisWidget.linkDecrease.addEventListener('click', function(){
+    thisWidget.dom.linkDecrease.addEventListener('click', function(){
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
 
-    thisWidget.linkIncrease.addEventListener('click', function(){
+    thisWidget.dom.linkIncrease.addEventListener('click', function(){
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
   }
+  /* method announce has been moved to class BaseWidget*/
 
-  announce(){
-    const thisWidget = this;
-
-    //const event =new Event('updated');
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-    thisWidget.element.dispatchEvent(event);
-  }
 }
 
 export default AmountWidget;
