88963d8ee3e2fbfbe3985dabed9cf3e6e3c5f3ff
Rado Jedro <radoslaw.jedro@gmail.com>
2019-10-06 23:03:32 +0100

    Create HourPicker.js

---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,47 @@
+/* global rangeSlider */
+import BaseWidget from './BaseWidget.js';
+import {settings,select} from '../settings.js';
+import {utils} from '../utils.js';
+
+class HourPicker extends BaseWidget{
+  constructor(wrapper){
+    /*must have super() and refers to class BaseWidget */
+    super(wrapper,settings.hours.open);
+
+    const thisWidget = this;
+
+    /* create attributes equal to single element find in thisWidget.dom.wrapper using selector saved in widget. */
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+
+    thisWidget.initPlugin();
+
+  }
+
+  initPlugin(){
+
+    const thisWidget = this;
+    rangeSlider.create(thisWidget.dom.input);
+
+    thisWidget.dom.input.addEventListener('input', function(){
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+  }
+
+  parseValue(value){
+    return utils.numberToHour(value);
+
+  }
+
+  isValid(){
+    return true;
+  }
+
+  renderValue(){
+    const thisWidget = this;
+    /* display widget value*/
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+  }
+}
+export default HourPicker;
